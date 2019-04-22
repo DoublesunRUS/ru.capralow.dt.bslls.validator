1a6f5d752dc78d6f950e29518e50fa1a0cfb81a6
Капралов Александр <capralow@gmail.com>
2019-04-22 23:14:32 +0300

    Ветка для демонстрации неработающего валидатора

--- ru.capralow.dt.bslls.validator.plugin.ui/src/ru/capralow/dt/bslls/validator/plugin/ui/BslValidator.java
+++ ru.capralow.dt.bslls.validator.plugin.ui/src/ru/capralow/dt/bslls/validator/plugin/ui/BslValidator.java
@@ -1,63 +1,53 @@
 package ru.capralow.dt.bslls.validator.plugin.ui;
 
 import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.DiagnosticSeverity;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
+import org.eclipse.xtext.resource.XtextResource;
 import org.github._1c_syntax.bsl.languageserver.context.ServerContext;
 import org.github._1c_syntax.bsl.languageserver.providers.DiagnosticProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com._1c.g5.v8.dt.bsl.model.Function;
 import com._1c.g5.v8.dt.bsl.model.Module;
-import com._1c.g5.v8.dt.bsl.model.Procedure;
 import com._1c.g5.v8.dt.bsl.validation.CustomValidationMessageAcceptor;
 import com._1c.g5.v8.dt.bsl.validation.IExternalBslValidator;
-import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.Document;
 
 public class BslValidator implements IExternalBslValidator {
 	private static final Logger LOGGER = LoggerFactory.getLogger(BslValidator.class);
 
 	private DiagnosticProvider diagnosticProvider;
-	private ServerContext bslServerContext;
 
 	public BslValidator() {
 		super();
 
 		diagnosticProvider = new DiagnosticProvider();
-		bslServerContext = new ServerContext();
 	}
 
 	@Override
 	public boolean needValidation(EObject object) {
-		return object instanceof Procedure || object instanceof Function || object instanceof Module;
+		return object instanceof Module;
 	}
 
 	@Override
 	public void validate(EObject object, CustomValidationMessageAcceptor messageAcceptor) {
-		Module module;
-		if (object instanceof Module)
-			module = (Module) object;
-		else
-			module = EcoreUtil2.getContainerOfType(object, Module.class);
-		String objectUri = module.getUniqueName();
-		// FIXME: Повторная расширенная проверка не приводит к перепроверке
-		if (!(object instanceof Module) && bslServerContext.getDocument(objectUri) != null)
-			return;
-
+		Module module = (Module) object;
 		ICompositeNode node = NodeModelUtils.findActualNodeFor(module);
+
+		String objectUri = module.getUniqueName();
 		String objectText = node.getText();
 
-		bslServerContext.addDocument(objectUri, objectText);
 		Document doc = new Document(objectText);
 
+		ServerContext bslServerContext = new ServerContext();
+		bslServerContext.addDocument(objectUri, objectText);
 		List<Diagnostic> diagnostics = diagnosticProvider.computeDiagnostics(bslServerContext.getDocument(objectUri));
 		for (Diagnostic diagnostic : diagnostics) {
 			Integer offset = 0;
